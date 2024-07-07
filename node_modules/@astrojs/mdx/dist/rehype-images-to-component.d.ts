035b18836c0cbd13da93197bad230e0c59f85c7b
Drewbadour <4530595+Drewbadour@users.noreply.github.com>
2024-07-06 20:23:09 -0500

    Fix the gitignore

--- node_modules/@astrojs/mdx/dist/rehype-images-to-component.d.ts
+++
@@ -1,6 +0,0 @@
-import type { MarkdownVFile } from '@astrojs/markdown-remark';
-import type { Root } from 'hast';
-export declare const ASTRO_IMAGE_ELEMENT = "astro-image";
-export declare const ASTRO_IMAGE_IMPORT = "__AstroImage__";
-export declare const USES_ASTRO_IMAGE_FLAG = "__usesAstroImage";
-export declare function rehypeImageToComponent(): (tree: Root, file: MarkdownVFile) => void;
--- node_modules/@astrojs/mdx/dist/vite-plugin-mdx.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Plugin } from 'vite';
-import type { MdxOptions } from './index.js';
-export declare function vitePluginMdx(mdxOptions: MdxOptions): Plugin;
